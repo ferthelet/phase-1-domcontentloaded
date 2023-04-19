b6637423e5cf14e9b6b43b6faee2a45040a8b462
Juan Salazar <ferthelet@gmail.com>
2023-04-19 01:46:09 -0500

    adds script after DOM's load, changes p text

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+// js
+// Use the event's callback function to target the paragraph element 
+// with id="text" and replace the text with "This is really cool!"
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("After the DOM has loaded");
+    text = document.querySelector('#text');
+    text.textContent = "This is really cool!";
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
